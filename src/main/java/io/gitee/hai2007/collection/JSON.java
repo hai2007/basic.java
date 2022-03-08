cde012cae8feb2ec77beb67283df24adcd133844
你好2007 <2501482523@qq.com>
2022-03-08 16:49:45 +0800

    新建JSON空文件

--- src/main/java/io/gitee/hai2007/collection/JSON.java
+++ src/main/java/io/gitee/hai2007/collection/JSON.java
@@ -2,6 +2,20 @@ package io.gitee.hai2007.collection;
 
 public class JSON {
 
-	
-	
+	public JSON(String str) {
+
+	}
+
+	public void setValue(String key, String value) {
+
+	}
+
+	public String getValue(String key) {
+		return "";
+	}
+
+	public String toString() {
+		return "";
+	}
+
 }
--- src/test/java/io/gitee/hai2007/collection/TestJSON.java
+++ src/test/java/io/gitee/hai2007/collection/TestJSON.java
@@ -3,5 +3,8 @@ package io.gitee.hai2007.collection;
 public class TestJSON {
 	public static void main(String[] args) {
 
+		JSON json = new JSON("{key1:{},key2:\"value2\",key3:[{key4:123},12]}");
+		System.out.println(json);
+
 	}
 }
